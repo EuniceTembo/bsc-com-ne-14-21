cbe6bc2f181a46a092069b3600ebf6d333d5ffe8
EuniceTembo <162663430+EuniceTembo@users.noreply.github.com>
2024-04-23 21:20:36 +0100

    Update

---
+++ array.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+string a[] = {"B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"};
+int m = sizeOf(a)/ sizeOf([a]);
+
+for(into i = 0; i < m; i++){
+ if(a[i].find('B') == 0){
+
+ count << a[i] << endl;
+
+}
+}
+}
